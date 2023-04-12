c05739fe5c6086758f583eacb193eb4c146556c6
Arsenii PC <origingoy@gmail.com>
2023-04-12 14:30:10 +0300

    renamed todo.go to todo_repo.go

--- internal/store/todo_repo.go
+++ internal/store/todo_repo.go
@@ -9,6 +9,12 @@ type ToDoRepo struct {
 	db *gorm.DB
 }
 
+func (td *ToDoRepo) FindById(id uint) (*models.ToDo, error) {
+	var todo models.ToDo
+	result := td.db.First(&todo, id)
+	return &todo, result.Error
+}
+
 func (td *ToDoRepo) GetByUser(userID uint) ([]*models.ToDo, error) {
 	var todos []*models.ToDo
 	result := td.db.Where("user_id = ?", userID).Find(&todos)
@@ -19,15 +25,10 @@ func (td *ToDoRepo) Create(todo *models.ToDo) error {
 	return td.db.Create(todo).Error
 }
 
-func (td *ToDoRepo) UpdateFull(todo *models.ToDo, id uint) error {
-	var oldTodo models.ToDo
-	result := td.db.First(&oldTodo, id)
-	if result.Error != nil {
-		return result.Error
-	}
+func (td *ToDoRepo) UpdateFull(updatedTodo *models.ToDo) error {
+	return td.db.Save(updatedTodo).Error
+}
 
-	oldTodo.Task = todo.Task
-	oldTodo.Completed = todo.Completed
-	todo.UserID = oldTodo.UserID
-	return td.db.Save(&oldTodo).Error
+func (td *ToDoRepo) Delete(id uint) error {
+	return nil
 }
