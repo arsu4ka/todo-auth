4cebb2f1a0f3e63d5657c314c2d8fc95dd66cd98
Arsenii <origingoy@gmail.com>
2023-08-24 23:47:27 +0300

    added verification and email services to handler

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -3,6 +3,8 @@ package handlers
 import "github.com/arsu4ka/todo-auth/internal/services"
 
 type RequestsHandler struct {
-	User services.IUserService
-	Todo services.ITodoService
+	User         services.IUserService
+	Todo         services.ITodoService
+	Verification services.IVerificationService
+	Email        services.IEmailService
 }
