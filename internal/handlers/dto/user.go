d0b2d23096a00b2af0c83dbc4f792789b79ab3a8
Arsenii <origingoy@gmail.com>
2023-08-26 12:19:06 +0300

    updated user dto after user model

--- internal/handlers/dto/user.go
+++ internal/handlers/dto/user.go
@@ -1,6 +1,10 @@
 package dto
 
-import "github.com/arsu4ka/todo-auth/internal/models"
+import (
+	"time"
+
+	"github.com/arsu4ka/todo-auth/internal/models"
+)
 
 type CreateUserDto struct {
 	FullName string `json:"full_name" binding:"required"`
@@ -14,15 +18,17 @@ type LoginUserDto struct {
 }
 
 type ResponseUserDto struct {
-	ID       uint   `json:"id"`
-	FullName string `json:"fullName"`
-	Email    string `json:"email"`
+	ID        uint      `json:"id"`
+	FullName  string    `json:"fullName"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"time"`
 }
 
 func NewResponseUserDto(user *models.User) *ResponseUserDto {
 	return &ResponseUserDto{
-		ID:       user.ID,
-		FullName: user.FullName,
-		Email:    user.Email,
+		ID:        user.ID,
+		FullName:  user.FullName,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
 	}
 }
