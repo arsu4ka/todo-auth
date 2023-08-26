b63ec0dbda93c70ca3a67c811d13f4b07688cd0c
Arsenii <origingoy@gmail.com>
2023-08-26 12:18:49 +0300

    added description, createdAt and updatedAt fields

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,10 +1,16 @@
 package models
 
+import "time"
+
 type Todo struct {
-	ID        uint
-	Task      string
-	Completed bool `gorm:"default:false"`
-	UserID    uint
+	ID          uint
+	Task        string `gorm:"type:string;not null"`
+	Description string `gorm:"type:text"`
+	Completed   bool   `gorm:"default:false;not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+
+	UserID uint
 }
 
 func (t *Todo) Validate() error {
