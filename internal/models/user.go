9b4986f82a5c1c3a8ec371026599a99e591622ba
Arsenii <origingoy@gmail.com>
2023-08-26 12:18:25 +0300

    added createdAt field and gorm validation

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,24 +1,41 @@
 package models
 
 import (
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/asaskevich/govalidator"
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID       uint
-	FullName string
-	Email    string `gorm:"unique"`
-	Active   bool   `gorm:"default:false"`
-	Password string
+	ID        uint
+	FullName  string `gorm:"not null"`
+	Email     string `gorm:"unique;not null"`
+	Active    bool   `gorm:"default:false;not null"`
+	Password  string `gorm:"not null"`
+	CreatedAt time.Time
 }
 
 func (u *User) Validate() error {
 	return validation.ValidateStruct(
 		u,
 		validation.Field(&u.Email, validation.Required, is.Email),
-		validation.Field(&u.FullName, validation.Required),
+		validation.Field(&u.FullName, validation.Required, validation.NewStringRule(func(fullName string) bool {
+			names := strings.Split(fullName, " ")
+			for _, name := range names {
+				rule1 := govalidator.IsAlpha(name)
+				rule2 := name != ""
+				rule3 := unicode.IsUpper(rune(name[0]))
+				if !(rule1 && rule2 && rule3) {
+					return false
+				}
+			}
+			return true
+		}, "must be a valid email address")),
 	)
 }
 
