cab3c202ee74b37b2228592adfab655f8d6a4e40
Arsenii (win10) <origingoy@gmail.com>
2023-09-12 20:58:22 +0200

    fixed typo in json attribute

--- internal/handlers/dto/user.go
+++ internal/handlers/dto/user.go
@@ -7,7 +7,7 @@ import (
 )
 
 type CreateUserDto struct {
-	FullName string `json:"full_name" binding:"required"`
+	FullName string `json:"fullName" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
